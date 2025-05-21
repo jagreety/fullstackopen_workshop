a5fa4644001de6c4708e46e824f2b24f3940f688
Jagreety <jhajagriti282@gmail.com>
2025-05-21 14:27:13 +0545

    pure_react completed

---
+++ part-1/pure_react/src/index.js
@@ -0,0 +1,16 @@
+let SayHello = (props) => {
+    return React.createElement('h1', {id:"myID"}, `Hello ${props.firstName}`)
+}
+let App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [React.createElement("div", {}, React.createElement(SayHello, {firstName: "Jagriti"}),
+        React.createElement("div", {}, React.createElement(SayHello, {firstName: "Smriti"})), 
+        React.createElement("div", {}, React.createElement(SayHello, {firstName: "Aakriti"})))]
+    );
+}
+let container = document.getElementById("root");
+let root = ReactDOM.createRoot(container);
+
+root.render(React.createElement(App,));
