c7a7566310161ca4898ba67f263f5a7fd99b314e
Jagreety <jhajagriti282@gmail.com>
2025-05-23 13:26:37 +0545

    feat: dev environment complete

--- part-1/pure_react/src/index.js
+++ part-1/pure_react/src/index.js
@@ -1,16 +1,31 @@
 let SayHello = (props) => {
-    return React.createElement('h1', {id:"myID"}, `Hello ${props.firstName}`)
-}
+  return React.createElement("h1", { id: "myID" }, `Hello ${props.firstName}`);
+};
 let App = () => {
-    return React.createElement(
+  return React.createElement(
+    "div",
+    {},
+
+    [
+      React.createElement(
         "div",
         {},
-        [React.createElement("div", {}, React.createElement(SayHello, {firstName: "Jagriti"}),
-        React.createElement("div", {}, React.createElement(SayHello, {firstName: "Smriti"})), 
-        React.createElement("div", {}, React.createElement(SayHello, {firstName: "Aakriti"})))]
-    );
-}
+        React.createElement(SayHello, { firstName: "Jagriti" }),
+        React.createElement(
+          "div",
+          {},
+          React.createElement(SayHello, { firstName: "Smriti" })
+        ),
+        React.createElement(
+          "div",
+          {},
+          React.createElement(SayHello, { firstName: "Aakriti" })
+        )
+      ),
+    ]
+  );
+};
 let container = document.getElementById("root");
 let root = ReactDOM.createRoot(container);
 
-root.render(React.createElement(App,));
+root.render(React.createElement(App));
